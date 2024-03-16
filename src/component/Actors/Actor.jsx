407e5cd0772c87e1de9123887546cf83f0becf66
dev_dimeji✔ <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2024-03-16 17:20:47 +0100

    Update Actor.jsx

--- src/component/Actors/Actor.jsx
+++ src/component/Actors/Actor.jsx
@@ -1,10 +1,4 @@
 
-//   return (
-//     <>
-//       <Grid container spacing={3}>
-//         <Grid item lg={5} xl={4}>
-//           <img
-//             className={classes.image}
 //             src={`https://image.tmdb.org/t/p/w780/${data?.profile_path}`}
 //             alt={data.name}
 //           />
