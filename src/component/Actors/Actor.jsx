bbdae203c79b1b9e71ac48e0ab5370211260492c
dev_dimeji✔ <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2024-03-16 17:21:04 +0100

    Update Actor.jsx

--- src/component/Actors/Actor.jsx
+++ src/component/Actors/Actor.jsx
@@ -1,11 +1,4 @@
 
-//             src={`https://image.tmdb.org/t/p/w780/${data?.profile_path}`}
-//             alt={data.name}
-//           />
-//         </Grid>
-//       </Grid>
-//     </>
-//   );
 // };
 
 // export default Actor;
